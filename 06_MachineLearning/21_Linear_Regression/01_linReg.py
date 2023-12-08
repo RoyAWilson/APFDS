c74da4fa81aa1a8005d223210d1de60086b9ac62
Roy Wilson <royawilson@hotmail.co.uk>
2023-12-08 15:48:01 +0000

    To end of lecture 22 Logistic Regression

--- 06_MachineLearning/21_Linear_Regression/01_linReg.py
+++ 06_MachineLearning/21_Linear_Regression/01_linReg.py
@@ -3,3 +3,61 @@ Section 6 Machine Learning
 Lecture 21 Linear regression: Supervised Learning Algorithm
 Using Scikit learning library
 '''
+
+# import required libraries:
+
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import matplotlib
+import matplotlib.pyplot as plt
+
+# NB Printing the array doesn't work without matplot imported
+# Load a dataset to work with.  Loads as a numpy array:
+
+data = datasets.load_diabetes()
+# print(data)
+diabetes_df = pd.DataFrame(data=data.data, columns=data.feature_names)
+diabetes_df['target'] = data.target
+# print(diabetes_df)
+# print(diabetes_df,)
+
+# Seperate dataset to dependant and independant varialbe.
+
+x = diabetes_df.iloc[:, :10]
+y = diabetes_df['target']
+# print(x)
+# split dataset 80:20 for training and testing
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+# print(x_train)
+# print(y_test)
+
+# create instance
+
+model = LinearRegression()
+model.fit(x_train, y_train)
+
+# Return array of predicted values:
+
+y_predict = model.predict(x_test)
+# print(y_predict)
+# print(y_test)
+
+# # Check accuracy - more in a later lecture
+
+plt.plot(y_test, y_predict, '.')
+x = np.linspace(0, 330, 100)
+y = x
+plt.plot(x, y)
+plt.savefig(r'..\Plots\PredictedVals.png')
+plt.title('Predicted against Actual')
+plt.show()
+plt.close()
+
+# Check coefficient and intercept:
+
+print(model.coef_)
+print(model.intercept_)
---
+++ 06_MachineLearning/22_LogidticRegression/02_LogisticRegression.py
@@ -0,0 +1,38 @@
+'''
+Section 6 Mchine Learning
+Lecture 22 Logistic Regression
+'''
+
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv(r'..\..\Data_Files\Titanic.csv')
+# print(df)
+df = df.drop(['PassengerId', 'Name', 'SibSp',
+             'Parch', 'Ticket', 'Fare', 'Cabin'], axis=1)
+df['Age'].fillna(df['Age'].median(), inplace=True)
+# S is the mode of the values in Embarked.
+df['Embarked'].fillna('S', inplace=True)
+# print(df.isnull().any())
+
+# Separate dependent and independent variables into y - dependant and x - independant
+
+y = df['Survived']
+x = df.drop(['Survived'], axis=1)
+
+# Produce dummy variables for sex and embarked:
+
+x = pd.get_dummies(x)
+
+# Split the datasets
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+lm = LogisticRegression()
+lm.fit(x_train, y_train)
+y_pred = lm.predict(x_test)
+print(y_pred)
+print(y_test)
